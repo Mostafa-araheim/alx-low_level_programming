91e8932e9b0c178ce139f1f1ab61cec2424fac99
mostafa-araheim <mostafa.araheim@gmail.com>
2023-03-16 14:17:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %u byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %u byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
