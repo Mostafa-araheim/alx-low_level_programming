c4bebfbd36622aa86f76cd72c8f093fb6106520b
mostafa-araheim <mostafa.araheim@gmail.com>
2023-03-16 13:46:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a libe with puts function
+ * return: 0 (success)
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle") :
+		return 0 ;
+}
+
