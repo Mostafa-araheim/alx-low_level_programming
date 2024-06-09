c8e54b4f0d7955e2fd96216840e8e63165ae74b4
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-09 20:58:45 +0300

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+* swap_int - takes 2 Integers and swaps
+* their values
+*
+* @a : first integer to be swapped
+* @b : second integer to be swapped
+* Return  - nothing
+*/
+void swap_int(int *a ,int *b)
+{
+*a = *a ^ *b;
+*b = *b ^ *a;
+*a = *a ^ *b;
+}
