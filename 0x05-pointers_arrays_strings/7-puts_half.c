4cff84df31b110792d9c2f03528a70aeea6294db
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-10 18:36:42 +0300

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of the array
+ * @s: the string to be printed
+ * Return: nothing
+ */
+void puts_half(char *s)
+{
+int i = 0 ,k;
+while(s[i++])
+{
+i++;
+}
+if (i % 2 == 0)
+{
+k = i / 2;
+}
+else {
+k = (i - 1) / 2;
+}
+for (; k < i ; k++)
+{
+_putchar(s[k]);
+}
+}
