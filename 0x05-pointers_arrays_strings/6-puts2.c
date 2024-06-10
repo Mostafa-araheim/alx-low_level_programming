2f75d1e735f0ed9db4b43a028ebd92efd01c1fdc
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-10 18:18:02 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - this functions prints multiple indices of 2 of a string
+ * @s: String to be printed
+ * Return: nothing
+ *
+ */
+void puts2(char *s)
+{
+int i = 0, index;
+while (s[i] != '\0')
+{
+i++;
+}
+for (index = 0; index < i; index += 2)
+{
+printf("%c" ,s[index]);
+}
+_putchar('\n'); 
+}
