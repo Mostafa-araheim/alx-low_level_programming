7441c9669f5aea1b5ad53faf13547e9700e2ad4a
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-10 17:46:07 +0300

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+int c = 0 ,i=0;
+char temp;
+while (s[c] != '\0')
+{
+c++;
+}
+while (i <= c/2)
+{
+temp = s[i];
+s[i] = s[c-i-1];
+s[c-i-1] = temp;
+i++;
+}
+}
