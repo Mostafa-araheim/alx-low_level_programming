370ab7e0548b5f7203e433e546ef23275734ba59
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-11 02:02:34 +0300

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,14 @@
  */
 void print_array(int *a ,int n)
 {
-    int i = 0;
-    for (;i < n ; i++)
-    {          
-        printf("%d" ,a[i]);
-        if (i != n-1)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+int i = 0;
+for (;i < n ; i++)
+{          
+printf("%d" ,a[i]);
+if (i != n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
 }
